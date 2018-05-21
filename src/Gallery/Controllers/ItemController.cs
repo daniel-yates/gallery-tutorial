d53f2ff2f8a9ccfee8a23e20778a988005398f31
Dan Yates <daniel.yates@idealshoppingdirect.co.uk>
2018-05-21 17:25:44 +0100

    Added item view

--- src/Gallery/Controllers/ItemController.cs
+++ src/Gallery/Controllers/ItemController.cs
@@ -7,12 +7,7 @@
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the item controller");
+            return View();
         }
     }
 }
