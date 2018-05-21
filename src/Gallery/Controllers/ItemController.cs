5c4827fc9d585fdd9f87ac444491cc74ac540366
Dan Yates <daniel.yates@idealshoppingdirect.co.uk>
2018-05-21 16:47:26 +0100

    Using directive inside namespace

--- src/Gallery/Controllers/ItemController.cs
+++ src/Gallery/Controllers/ItemController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Gallery.Controllers
+﻿namespace Gallery.Controllers
 {
+    using System.Web.Mvc;
+
     public class ItemController : Controller
     {
         public string Detail()
