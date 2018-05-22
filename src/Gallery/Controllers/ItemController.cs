82fefebc024cc2cee26b231af331a83bd38f7b64
Dan Yates <daniel.yates@idealshoppingdirect.co.uk>
2018-05-22 16:48:39 +0100

    Updated ItemController and Detail view

--- src/Gallery/Controllers/ItemController.cs
+++ src/Gallery/Controllers/ItemController.cs
@@ -7,6 +7,16 @@
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "Street Fighter";
+            ViewBag.GameName = "Street Fighter 2: Champion Edition";
+            ViewBag.Description = "<p>Street Fighter II: Champion Edition is a competitive fighting game released for the arcades by Capcom in 1992</p>";
+            ViewBag.Developers = new string[]
+            {
+                "Producer(s): Yoshiki Okamoto",
+                "Designer(s): Akira Nishitani, Akira Yasuda",
+                "Composer(s): Isao Abe"
+            };
+
             return View();
         }
     }
