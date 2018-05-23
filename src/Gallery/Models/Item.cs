1046832934f4eb79f2bfe539a5bc03d57d625baf
Dan Yates <daniel.yates@idealshoppingdirect.co.uk>
2018-05-23 12:56:23 +0100

    Updated Item model

--- src/Gallery/Models/Item.cs
+++ src/Gallery/Models/Item.cs
@@ -13,5 +13,21 @@ namespace Gallery.Models
         public string DescriptionHtml { get; set; }
         public Developer[] Developers { get; set; }
         public bool Favourite { get; set; }
+
+        public string DisplayText {
+            get
+            {
+                return SeriesTitle + ": " + GameName;
+            }
+        }
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + GameName + ".jpg";
+            }
+        }
     }
 }
