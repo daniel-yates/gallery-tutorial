5d3e0d61488be028121f528b5a489e2bf956b552
Dan Yates <daniel.yates@idealshoppingdirect.co.uk>
2018-05-23 12:05:30 +0100

    Added Item and Developer models

---
+++ src/Gallery/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gallery.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public string GameName { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Developer[] Developers { get; set; }
+        public bool Favourite { get; set; }
+    }
+}
